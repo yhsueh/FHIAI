db75ee80b2ab82e0e71b7e3d58f5056bfe0627a0
Yuyu Hsueh <joehsueh@hotmail.com>
2021-04-24 21:28:51 +0800

    4_24

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import yolo
 import os
+import glob
 from PIL import Image
 
 dirname = os.path.dirname(__file__)
-image_name = 'File_007.jpeg'
-image_path = os.path.join(dirname, 'test_image/' + image_name)
 
-image = Image.open(image_path)
-yolo = yolo.YOLO()
-r_image = yolo.detect_image(image=image)
-save_path = os.path.join(dirname, 'output_image/' + image_name)
-r_image.save(save_path)
+
+def detect_image(path):
+    image = Image.open(path)
+    yolo_obj = yolo.YOLO()
+    r_image = yolo_obj.detect_image(image=image)
+    save_path = os.path.join(dirname, 'output_image/' + path[-5:])
+    r_image.save(save_path)
+
+
+def main():
+    for path in glob.glob('test_image/*.jpg'):
+        image_path = os.path.join(dirname, path)
+        detect_image(image_path)
+
+
+if __name__ == '__main__':
+    main()
